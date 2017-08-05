add349652621844921d5c1a28f29aa34cb46caba
Loaner <some@example.com>
2017-08-05 16:00:41 -0400

    ninth commit

--- marvel.js
+++ marvel.js
@@ -3,7 +3,7 @@ var myPicture = vRViewPlayer('#spiderman',{
   is_stereo: false
 });
 window.addEventListener('load', myPicture);
-myPicture.on('read', function(event) {
+myPicture.on('ready', function(event) {
   myPicture.addHotspot('creating-hotspot-1', {
     pitch: 0,
     yaw: 0,
