4873cafb6ca44fcf8f18d2fd6915055e6d1757c9
Loaner <some@example.com>
2017-08-05 15:53:35 -0400

    seventh commit

--- marvel.js
+++ marvel.js
@@ -3,3 +3,11 @@ var myPicture = vRViewPlayer('#spiderman',{
   is_stereo: false
 });
 window.addEventListener('load', myPicture);
+myPicture.on('read', function(event) {
+  myPicture.addHotspot('creating-hotspot-1', {
+    pitch: 0,
+    yaw: 0,
+    radius: 0.10
+    distance: 1
+  });
+});
